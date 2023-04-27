b2876feba3625a649e17c1f3fefaede7a777ea0e
Sham Targe <67789796+shamtarge@users.noreply.github.com>
2023-04-27 15:12:50 +0530

    Camera Capture Add and formatting updated

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ import torch
 from torchvision import models, transforms
 
 
-options = ['',"capsule", "carpet", "hazelnut", "leather", "metal_nut", "pill", "tile", "wood"]
+options = ['',"Capsule", "Carpet", "Hazelnut", "Leather", "Metal Nut", "Pill", "Tile", "Wood"]
 selected_option = ""
 
 def evaluate(input_img, object):
-    if(object == "capsule"):
+    if(object == "Capsule"):
         model = torch.load("models/capsule_model.h5")
-    elif(object == "carpet"):
+    elif(object == "Carpet"):
         model = torch.load("models/carpet_model.h5")
-    elif(object == "hazelnut"):
+    elif(object == "Hazelnut"):
         model = torch.load("models/hazelnut_model.h5")
-    elif(object == "leather"):
+    elif(object == "Leather"):
         model = torch.load("models/leather_model.h5")
-    elif(object == "metal_nut"):
+    elif(object == "Metal Nut"):
         model = torch.load("models/metal_nut_model.h5")
-    elif(object == "pill"):
+    elif(object == "Pill"):
         model = torch.load("models/pill_model.h5")
-    elif(object == "tile"):
+    elif(object == "Tile"):
         model = torch.load("models/tile_model.h5")
-    elif(object == "wood"):
+    elif(object == "Wood"):
         model = torch.load("models/wood_model.h5")
     else:
         return
@@ -41,64 +41,35 @@ def evaluate(input_img, object):
 
     # Analyze the output to determine whether the image is good or bad
     if preds[0][0] > 0.5:
-        st.write('This is a good image.')
+        st.success('Object does not have Defect')
     else:
-        st.write('This is a bad image.')
+        st.error('Object has Defects')
 
 
 def main():
-    st.title("Good Checker")
+    st.title("Defect Detector")
 
-    st.write("It will check if the given object in the image is defective of not")
+    st.write("Checks whether given object is Defective or Non-Defective")
 
-    selected_option = st.selectbox("Select object to test", options)
+    selected_option = st.selectbox("Select an Object", options)
 
     if selected_option:
         st.write("Object for testing is : ", selected_option)
         uploaded_file = None
-
+        captured_file = None
+        st.caption("Capture Image using Camera")
+        if st.button("Open Camera"):
+            captured_file = st.camera_input("Capture Image")
+            st.write("Image captured")
+            # captured_file = Image.open(capture)
+            if captured_file :
+                # st.write("Inside capt file")
+                if st.button("evaluate"):
+                    # st.write("Inside evaluate")
+                    evaluate(captured_file, selected_option)
+        st.write("OR")
         uploaded_file = st.file_uploader("Choose an image...", type="png")
         if uploaded_file is not None:
-            # image = Image.open(uploaded_file)
-            # image = image.resize((300, 300))
-            # st.image(image, caption="Uploaded image")
-
-            # # Load the model from an H5 file
-            # if(selected_option == "capsule"):
-            #     model = torch.load("models/capsule_model.h5")
-            # elif(selected_option == "carpet"):
-            #     model = torch.load("models/carpet_model.h5")
-            # elif(selected_option == "hazelnut"):
-            #     model = torch.load("models/hazelnut_model.h5")
-            # elif(selected_option == "leather"):
-            #     model = torch.load("models/leather_model.h5")
-            # elif(selected_option == "metal_nut"):
-            #     model = torch.load("models/metal_nut_model.h5")
-            # elif(selected_option == "pill"):
-            #     model = torch.load("models/pill_model.h5")
-            # elif(selected_option == "tile"):
-            #     model = torch.load("models/tile_model.h5")
-            # elif(selected_option == "wood"):
-            #     model = torch.load("models/wood_model.h5")
-            # else:
-            #     return
-
-            # # Preprocess the image
-            # img = Image.open(uploaded_file)
-            # img = transforms.Resize((224, 224))(img)
-            # img = transforms.ToTensor()(img)
-            # img = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
-
-            # # Pass the image through the model and get the predictions
-            # with torch.no_grad():
-            #     preds = model.forward(img.unsqueeze(0))
-
-            # # Analyze the output to determine whether the image is good or bad
-            # if preds[0][0] > 0.5:
-            #     st.write('This is a good image.')
-            # else:
-            #     st.write('This is a bad image.')
-
             if st.button("evaluate"):
                 evaluate(uploaded_file, selected_option)
 
